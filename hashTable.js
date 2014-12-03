580ef0cf7a00df78a911d546f8c4b2f9bff89145
Ariadna Bukatina <book_a-tina@bk.ru>
2014-12-04 00:09:55 +0600

    Add function simpleHash

--- hashTable.js
+++ hashTable.js
@@ -1,3 +1,12 @@
 function HashTable() {
 	this.table = new Array(140);
+	this.simpleHash = simpleHash;
+}
+
+function simpleHash(data) {
+	var total = 0;
+	for (var i = 0; i < data.length; ++i) {
+		total += data.charCodeAt(i);
+	}
+	return total % this.table.length;
 }
