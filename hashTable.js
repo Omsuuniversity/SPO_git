4d5b0945d614817c00c84ae80e6f92233b69cc92
Ariadna Bukatina <book_a-tina@bk.ru>
2014-12-04 00:06:25 +0600

    Add function put

--- hashTable.js
+++ hashTable.js
@@ -1,3 +1,17 @@
 function HashTable() {
 	this.table = new Array(140);
+	this.simpleHash = simpleHash;
+	this.put = put;
+}
+
+function put(data) {
+	var pos = this.simpleHash(data);
+	this.table[pos] = data;
+}
+function simpleHash(data) {
+	var total = 0;
+	for (var i = 0; i < data.length; ++i) {
+		total += data.charCodeAt(i);
+	}
+	return total % this.table.length;
 }
